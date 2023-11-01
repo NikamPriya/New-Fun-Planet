570f554df31257918ae230f1104deafb2f2c1dc6
hirkani <hirkanibhosale2001@gmail.com>
2023-11-01 16:40:13 +0530

    constant changes

---
+++ src/app/core/constant/APIConstant.ts
@@ -0,0 +1,12 @@
+export const APIConstant = {
+
+ Resort:{
+    get:"https://freeapi.miniprojectideas.com/api/Resort/GetAllService",
+    Create:"https://freeapi.miniprojectideas.com/api/Resort/AddService",
+    Delete:"https://freeapi.miniprojectideas.com/api/Resort/DeleteService"
+
+ }
+
+ 
+
+}
