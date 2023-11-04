898ad3da871dffacb86e47f1508c891692ba071c
shweta <shwetakapase9@gmail.com>
2023-11-04 17:09:49 +0530

    wedding service chnages

---
+++ src/app/core/constant/APIConstant.ts
@@ -0,0 +1,9 @@
+export const APIConstant = {
+    weddingServices:{
+        getWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/GetweddingService',
+        addWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddweddingService',
+        updateWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddUpdateBulkWeddingService',
+        deleteWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/DeleteweddingService'
+    }
+
+}
