d28aeb27f8fb9307bc82a55c8e079b7eb166d7b9
shweta <shwetakapase9@gmail.com>
2023-11-05 17:59:29 +0530

    changes done

--- src/app/core/constant/APIConstant.ts
+++ src/app/core/constant/APIConstant.ts
@@ -1,28 +1,28 @@
 export const APIConstant = {
-   weddingServices:{
-        getWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/GetweddingService',
-        addWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddweddingService',
-        updateWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddUpdateBulkWeddingService',
-        deleteWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/DeleteweddingService'
-    },
+   weddingServices: {
+      getWeddingServices: 'https://freeapi.miniprojectideas.com/api/Resort/GetweddingService',
+      addWeddingServices: 'https://freeapi.miniprojectideas.com/api/Resort/AddweddingService',
+      updateWeddingServices: 'https://freeapi.miniprojectideas.com/api/Resort/AddUpdateBulkWeddingService',
+      deleteWeddingServices: 'https://freeapi.miniprojectideas.com/api/Resort/DeleteweddingService'
+   },
 
- Resort:{
-    get:"https://freeapi.miniprojectideas.com/api/Resort/GetAllService",
-    Create:"https://freeapi.miniprojectideas.com/api/Resort/AddService",
-    Delete:"https://freeapi.miniprojectideas.com/api/Resort/DeleteService"
+   Resort: {
+      get: "https://freeapi.miniprojectideas.com/api/Resort/GetAllService",
+      Create: "https://freeapi.miniprojectideas.com/api/Resort/AddService",
+      Delete: "https://freeapi.miniprojectideas.com/api/Resort/DeleteService"
 
- },
- weddingPackages: {
-   getWeddingPackages: "https://freeapi.miniprojectideas.com/api/Resort/GetAllWeddingPackages",
-   createWeddingPackage: "https://freeapi.miniprojectideas.com/api/Resort/CreateUpdateWeddingPackage",
-   deleteWeddingPackage: "https://freeapi.miniprojectideas.com/api/Resort/DeleteWeddingPackageById?id="
-},
-foodItemType:{
-   getAllFoodItemType:"https://freeapi.miniprojectideas.com/api/Resort/GetAllFoodItemType",
-   addFoodItemType:"https://freeapi.miniprojectideas.com/api/Resort/AddFoodItemtype"
-}
+   },
+   weddingPackages: {
+      getWeddingPackages: "https://freeapi.miniprojectideas.com/api/Resort/GetAllWeddingPackages",
+      createWeddingPackage: "https://freeapi.miniprojectideas.com/api/Resort/CreateUpdateWeddingPackage",
+      deleteWeddingPackage: "https://freeapi.miniprojectideas.com/api/Resort/DeleteWeddingPackageById?id="
+   },
+   foodItemType: {
+      getAllFoodItemType: "https://freeapi.miniprojectideas.com/api/Resort/GetAllFoodItemType",
+      addFoodItemType: "https://freeapi.miniprojectideas.com/api/Resort/AddFoodItemtype"
+   }
 
 
- 
+
 
 }
