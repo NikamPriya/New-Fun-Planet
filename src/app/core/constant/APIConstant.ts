07a467f1e004a6c3db2cae67830f415cac4bf5fb
shweta <shwetakapase9@gmail.com>
2023-11-05 13:11:37 +0530

    foot-type

--- src/app/core/constant/APIConstant.ts
+++ src/app/core/constant/APIConstant.ts
@@ -1,13 +1,10 @@
 export const APIConstant = {
-<<<<<<< HEAD
-=======
     weddingServices:{
         getWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/GetweddingService',
         addWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddweddingService',
         updateWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddUpdateBulkWeddingService',
         deleteWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/DeleteweddingService'
     },
->>>>>>> a08cd21bfca34e4dc7e56e40389c7533426540e1
 
  Resort:{
     get:"https://freeapi.miniprojectideas.com/api/Resort/GetAllService",
@@ -19,12 +16,6 @@ export const APIConstant = {
    getWeddingPackages: "https://freeapi.miniprojectideas.com/api/Resort/GetAllWeddingPackages",
    createWeddingPackage: "https://freeapi.miniprojectideas.com/api/Resort/CreateUpdateWeddingPackage",
    deleteWeddingPackage: "https://freeapi.miniprojectideas.com/api/Resort/DeleteWeddingPackageById?id="
-},
-weddingServices:{
-    getWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/GetweddingService',
-    addWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddweddingService',
-    updateWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/AddUpdateBulkWeddingService',
-    deleteWeddingServices:'https://freeapi.miniprojectideas.com/api/Resort/DeleteweddingService'
 }
 
 
